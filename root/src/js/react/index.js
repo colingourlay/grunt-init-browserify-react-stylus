20cf572240dbe14fe016786b864c885bea67790c
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-12-05 10:39:42 +1000

    Target more compact React module.

--- root/src/js/react/index.js
+++ root/src/js/react/index.js
@@ -1,6 +1,7 @@
+// This is
 module.exports = {
     // Expose React
-    React: require('react-tools').React,
+    React: require('react-tools/build/modules/React'),
     // Expose React, including React.addons.*
     ReactWithAddons: require('react-tools/build/modules/ReactWithAddons')
 };
